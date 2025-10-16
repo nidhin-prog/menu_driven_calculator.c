77dc94dc1a2b24c0bfdc70294ae9bcf5f327bd10
nidhinandepalli <nidhi.nn307@gmail.com>
2025-10-16 20:03:36 +0530

    Add files via upload

---
+++ Cexp.1.c
@@ -0,0 +1,15 @@
+/*Name: Nidhi Nandepalli
+ Class, Div: Comp2/D
+ UIN: 251P092 */
+
+#include <stdio.h>
+int main() {
+
+printf("My name is Nidhi \n");
+printf("My age is 18 \n");
+printf("My division is D \n");
+printf("My class is Comp 2 \n");
+printf("My UIN is 251P092 \n");
+
+return 0;
+}
